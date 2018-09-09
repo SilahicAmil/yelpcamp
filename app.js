310dbec4762d76e061ac264717492319dc3499f0
Amil Silahic <jinobie02@gmail.com>
2018-09-09 23:31:59 +0000

    styling edits  v0.01

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-// mongoose.connect("mongodb://localhost/yelp_camp_v10");
-mongoose.connect("mongodb://DesignateArc:Scabby123@ds151382.mlab.com:51382/yelpcamp");
+mongoose.connect("mongodb://localhost/yelp_camp_v10");
+// mongoose.connect("mongodb://DesignateArc:Scabby123@ds151382.mlab.com:51382/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
